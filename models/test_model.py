3f131d95e49a0a763204c043acf49868f3b0c14b
KhecharaSaiVenkataramana <luckyvenkat256@gmail.com>
2026-03-19 14:56:04 +0530

    Remove models folder from tracking

--- models/test_model.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# DOTA model
-dota_model = YOLO("dota_model.pt")
-dota_model.predict("image.jpg", save=True)
-
-# HRSC model
-hrsc_model = YOLO("hrsc_model.pt")
-hrsc_model.predict("ship_image.jpg", save=True)
